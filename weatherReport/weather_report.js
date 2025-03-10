6e0b25e74f9ccb9fbb954ea0579d323e94fc3ecb
Aynur <markmarkovm@gmail.com>
2025-03-10 06:58:43 -0400

    add weather report

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,21 @@
+function showWeatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = '179d0d9d17bd67ff3612101b47d7e56f';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    }).catch(error => {
+        console.error('Ошибка при получении погоды:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Не удалось получить погоду. Пожалуйста, попробуйте снова.</p>`;
+    });
+};
+
+document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails);
